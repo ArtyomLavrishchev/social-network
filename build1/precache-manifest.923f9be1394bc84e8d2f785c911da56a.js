bb5cf557adeea5e648e300cb2cb978a17fffc7d7
ArtyomLavrishchev <temalggt540@gmail.com>
2020-07-24 22:19:59 +0300

    added react app files

---
+++ build1/precache-manifest.923f9be1394bc84e8d2f785c911da56a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e151068ac3a3689680cb6ae67473d906",
+    "url": "/react-way-of-samurai/index.html"
+  },
+  {
+    "revision": "d7bf5932c777a0ed205e",
+    "url": "/react-way-of-samurai/static/css/main.56e12d6a.chunk.css"
+  },
+  {
+    "revision": "4a403143a409b1bae576",
+    "url": "/react-way-of-samurai/static/js/2.df5a7b73.chunk.js"
+  },
+  {
+    "revision": "d7bf5932c777a0ed205e",
+    "url": "/react-way-of-samurai/static/js/main.275981b0.chunk.js"
+  },
+  {
+    "revision": "e9ac23790aafa4fa0efc",
+    "url": "/react-way-of-samurai/static/js/runtime~main.c6fc2095.js"
+  }
+]);

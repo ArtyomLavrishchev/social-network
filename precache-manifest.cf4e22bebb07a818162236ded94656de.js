0a231475d411112a896bdab61a25f1be5f2f0622
ArtyomLavrishchev <temalggt540@gmail.com>
2020-09-13 23:44:51 +0300

    Updates

---
+++ precache-manifest.cf4e22bebb07a818162236ded94656de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5948aaf623a223d1b10e18cb7b2a82cf",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "c0bc979fc5154d4a1721",
+    "url": "/social-network/static/css/main.c937c426.chunk.css"
+  },
+  {
+    "revision": "bb38e01036162443cc54",
+    "url": "/social-network/static/js/2.3ab4b131.chunk.js"
+  },
+  {
+    "revision": "a1a88636b752ef668fe9699df3c2d360",
+    "url": "/social-network/static/js/2.3ab4b131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0bc979fc5154d4a1721",
+    "url": "/social-network/static/js/main.d3b8e8ba.chunk.js"
+  },
+  {
+    "revision": "cc3f02edcd0a386c5bd0",
+    "url": "/social-network/static/js/runtime-main.fca6ac10.js"
+  },
+  {
+    "revision": "dc9a522218b00866ec073aa46a8a9380",
+    "url": "/social-network/static/media/loader.dc9a5222.svg"
+  },
+  {
+    "revision": "b3e90dfeb6b3b2de52eddd2c94ab19ab",
+    "url": "/social-network/static/media/user-profile-icon-png.b3e90dfe.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-network/precache-manifest.50fa015966aa559c01bdbea2c948de8b.js"
+  "/social-network/precache-manifest.cf4e22bebb07a818162236ded94656de.js"
 );
 
 self.addEventListener('message', (event) => {
